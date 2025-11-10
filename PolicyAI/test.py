e3c18496e812b0e17f3bd4270e9bbdd0249cf96c
Yihan Zhao <yihan1322@gmail.com>
2025-11-10 17:16:46 +0000

    Deleted wrong document

--- PolicyAI/test.py
+++
@@ -1,5 +0,0 @@
-import FaissSearch as FS
-
-items, texts = FS.load_sentences("C:/Users/lenovo/Desktop/SEFB/MyGitTest/PolicyAI/imperial_policies.jsonl")
-index, _ = FS.build_index(texts)
-print(FS.search(index, "late submission penalty", texts, k=5))
